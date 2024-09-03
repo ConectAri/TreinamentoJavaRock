bb63bf2ea15b9a42acb5d0b8dbbe0b7fbf0b0ccc
Ariane Moura <ariane.barboza@mercadolivre.com>
2024-09-03 15:12:40 -0300

    Arrays

---
+++ atividadesRocketseat/src/main/java/com/arimoura/rocketseat/atividadesRocketseat/je10_expressoes/ExpressoesSimples.java
@@ -0,0 +1,16 @@
+package com.arimoura.rocketseat.atividadesRocketseat.je10_expressoes;
+
+public class ExpressoesSimples {
+
+    public static void main(String[] args) {
+
+        String nome = " Ana Paula";
+        String textoImpresso = "Ola" .concat(nome);
+
+
+
+
+    }
+
+
+}
---
+++ atividadesRocketseat/src/main/java/com/arimoura/rocketseat/atividadesRocketseat/je11_array/Arrays.java
@@ -0,0 +1,25 @@
+package com.arimoura.rocketseat.atividadesRocketseat.je11_array;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        Integer [] array = new Integer[6];
+        array[0] = 2;
+        array[1] = 4;
+        array[2] = 10;
+        array[3] = 15;
+        array[4] = 5;
+        array[5] = 3;
+
+        for(int i=0; i<array.length; i++){
+            int elemento = array [i];
+            System.out.println(elemento);
+        }
+
+
+
+    }
+
+
+}
